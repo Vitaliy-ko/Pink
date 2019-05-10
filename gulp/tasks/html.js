cb73078d194000b4872de59bae943d324ebd60a6
Vitaliy Kalenichenko <vitaliy.kalenichenko@gmail.com>
2019-05-10 20:17:54 +0300

    Remove gulp-post-html plugin

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,9 +1,7 @@
 const { src, dest } = require("gulp");
-const posthtml = require('gulp-posthtml');
 
 module.exports = function () {
    return function html() {
-      const include = [ require('posthtml-include')({ root: '' }) ]
       return src('./src/**/*.html')
          .pipe(dest('./build/'))
          .on('end', $.browserSync.reload);
